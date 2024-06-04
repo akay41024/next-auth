b60ab900eb3752a5058725ab0a6904c0038a2699
Aasif khan <akay41024@gmail.com>
2024-06-05 04:40:34 +0530

    changes commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import Button from '../../components/themeBtn'
+import Button from "../../components/themeBtn";
 
 export default function ProfilePage() {
   const router = useRouter();
@@ -27,32 +27,34 @@ export default function ProfilePage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Button/>
-      <h1>Profile</h1>
-      <hr />
-      <p>Profile page</p>
-      <h2 className="p-1 rounded bg-green-500">
-        {data === "nothing" ? (
-          "Nothing"
-        ) : (
-          <Link href={`/profile/${data}`}>{data}</Link>
-        )}
-      </h2>
-      <hr />
-      <button
-        onClick={logout}
-        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Logout
-      </button>
+    <>
+      <Button />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-black bg-white">
+        <h1 className="dark:text-white text-black">Profile</h1>
+        <hr />
+        <p className="dark:text-white text-black">Profile page</p>
+        <h2 className="p-1 rounded bg-green-500">
+          {data === "nothing" ? (
+            "Nothing"
+          ) : (
+            <Link href={`/profile/${data}`}>{data}</Link>
+          )}
+        </h2>
+        <hr />
+        <button
+          onClick={logout}
+          className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Logout
+        </button>
 
-      <button
-        onClick={getUserDetails}
-        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        GetUser Details
-      </button>
-    </div>
+        <button
+          onClick={getUserDetails}
+          className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          GetUser Details
+        </button>
+      </div>
+    </>
   );
 }
